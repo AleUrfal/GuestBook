5eae9cf72d75c51a0d9cd58cb6985d05c0ce5c82
AleUrfal <66021966+AleUrfal@users.noreply.github.com>
2022-01-08 16:13:06 +0100

    Zrobione większość
    Git jest

---
+++ messages.php
@@ -0,0 +1,23 @@
+<table>
+		<tr> <th style="width:10%;"> Autor </th> <th>Wiadomość</th> <th style="width:10%;">Data</th> </tr> 
+
+		<?php
+            include "authenticate.php";
+			include "config.php";
+			$sql_statement = "SELECT * FROM messages";
+
+			$result = mysqli_query($db, $sql_statement);
+
+			
+			while($row = mysqli_fetch_assoc($result))
+			{
+				$id = $row['id'];
+				$mysender = $row['sender'];
+				$message = $row['message'];
+				$data = $row['data'];
+
+				echo "<tr>". "<th>" . $id . "<th>" . $mysender . "</th>" . "<th>" . $message . "</th>"  . "<th>" . $data . "</th>" . "</tr>";
+			}
+			
+		?>
+	</table>
