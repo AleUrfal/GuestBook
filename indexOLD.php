dc4dbda8287ca7f3fec610514c71a14019ca0d02
malikmarcel <marcell2k@gmail.com>
2022-01-08 17:34:44 +0100

    Postep

--- indexOLD.php
+++ indexOLD.php
@@ -4,35 +4,23 @@
 	<meta charset="UTF-8">
 	<title>Księga Gości</title>
 	<link rel="stylesheet" href="style.css"/>
-
 </head>
 
 <body>
 		<h1><b>Witaj w naszej Księdze Gości</b></h1>
 		<br>
 		<h2>Pozostaw po sobie wiadomość</h2>
-		<h1>dasd</h1>
 		<form action="sendmsg.php" method="POST">
 			<input type="text" name="name" placeholder="Imię"><br>
 			<input type="text" name="message" placeholder="Wiadomość" rows="5" width="90%"></textarea><br>
 			<button class="postButton">DODAJ</button>
 		</form>
 
-		<hr>
-
 		<br>
 		<br>
 		<h2>Tutaj możesz przeczytać wszystkie wpisy:</h2>
-		<!-- Poczatek ksiazki html -->
-
-		<div class="center">
-            <div class="page">
-                <h1>Księga Gości</h1>
-       
-
-		<!-- Tabel zostawiam -->
 		<table>
-		<tr> <th style="width:10%;"> Autor </th> <th style="width:30%"> Wiadomość </th> <th style="width:5%;">Data</th> </tr> 
+		<tr> <th style="width:10%;"> Autor </th> <th>Wiadomość</th> <th style="width:10%;">Data</th> </tr> 
 
 		<?php
 			include "config.php";
@@ -48,17 +36,10 @@
 				$message = $row['message'];
 				$data = $row['data'];
 
-				echo "<tr>" . "<th>" . $mysender . "<hr>" . "</th>" . "<th>" . $message . "<hr>" . "</th>"  . "<th>" . $data . "<hr>" . "</th>" . "</tr>";
+				echo "<tr>" . "<th>" . $mysender . "</th>" . "<th>" . $message . "</th>"  . "<th>" . $data . "</th>" . "</tr>";
 			}
 			
 		?>
 	</table>
-	<!-- Tabel zostawiam -->
-
-	   </div>
-        </div>
-
-	<!-- koniec ksiazki html -->
-
 </body>
 </html>
