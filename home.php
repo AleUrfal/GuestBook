6855ca90f5cc0ee4dc80687de305c21c7de56dee
AleUrfal <66021966+AleUrfal@users.noreply.github.com>
2022-01-20 20:24:28 +0100

    end ?

--- home.php
+++ home.php
@@ -33,18 +33,52 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 		echo "<option value=$id>".$id."</option>";
 	}
 ?>
-<!--
-<div id="edit-container">
-<a id="edit-text">Wybierz id komentarza który chcesz edytować </a>
-<form action="" method="POST">
-<select name="eds">
-/*
 
--->
 </select></br>
 	<button id="delete-butt">DELETE</button>
 </form>
+</br>
+<!--
+<div id="edit-container">
+<a id="edit-text">Wybierz id komentarza który chcesz edytować </a>
+<form action="sendadmin.php" method="POST">
+<select name="eds">
+<?php
+	include "config.php";
+
+	$sql_command = "SELECT id FROM messages";
+	$myresult = mysqli_query($db, $sql_command);
+
+	while($id_rows = mysqli_fetch_assoc($myresult))
+	{
+		$id1 = $id_rows['id'];
+		echo "<option value=$id1>".$id1."</option>";
+	}
+?>
+<?php
+if (isset($_POST['eds']))
+{
+	$selection_id = $_POST['eds'];
+	$newvalue=$_POST['wiadomosc'];
+	$sql_statement = "INSERT INTO messages(messeage) VALUES = '$newvalue' WHERE id = $selection_id";
+	$result = mysqli_query($db, $sql_statement);
+
+	header ("Location: home.php");
+}
+else
+{
+	echo "NIE ZADZIAŁAŁO";
+}
+?>
+</select></br>
+<a id="edit-text">Wpisz tutaj swój tekst</a>
+<input type="text" name="wiadomosc" placeholder="Tu wpisz swoja wiadomosc" require><br>
+	<button id="edit-butt">Edit</button>
+</form>
+-->
+
 </div>
+</br>
 <div id="lock">
 	 <a href="logout.php"><img id ="lock-icon" src="icons/lock.png"></a>
 	</div>
